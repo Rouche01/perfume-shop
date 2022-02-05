896da57ff52ab8d389f34ae70b9ca6ad800fe2e7
Richard Emate <richardemate@gmail.com>
2022-02-05 18:23:25 +0100

    created map component

--- pages/contact-us.tsx
+++ pages/contact-us.tsx
@@ -1,9 +1,28 @@
+import Head from "next/head";
 import React from "react";
 import styled from "styled-components";
-import { PageContainer } from "../src/generalStyles";
+import Map from "../src/components/Map";
+import { PageContainer, PageTitle } from "../src/generalStyles";
 
 const ContactUs = () => {
-  return <PageContainer></PageContainer>;
+  return (
+    <PageContainer>
+      <Head>
+        <title>Peace Luxury | Contact Us</title>
+        <meta name="description" content="Authentic Perfumes & Ouds" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <PageTitle>Contact Us</PageTitle>
+      <Map
+        location={{
+          address: "Addo Road, Ajah, Lagos, Nigeria",
+          lat: 6.466667,
+          lng: 3.566667,
+        }}
+        zoomLvl={12}
+      />
+    </PageContainer>
+  );
 };
 
 export default ContactUs;
